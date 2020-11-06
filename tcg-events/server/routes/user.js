003adf760332ca0e4a48b30978c49872669718e6
TCG <wonganiskosana@gmail.com>
2020-11-06 09:56:09 +0200

    add the server fold and client

--- tcg-events/server/routes/user.js
+++ tcg-events/server/routes/user.js
@@ -1,20 +1,53 @@
-
-
-const User = require('../models/User')
+const User = require("../models/User");
 const appRoute = (app) => {
+  const events = [
+    {
+      id: 0,
+      name: "Year end party",
+      date: "2020-12-20",
+      venue: "Riversands Incubation hub",
+    },
+    {
+      id: 1,
+      name: "Christmas Eve",
+      date: "2020-12-24",
+      venue: "Riversands Incubation hub",
+    },
+    {
+      id: 2,
+      name: "New years Eve",
+      date: "2020-12-31",
+      venue: "Riversands Incubation hub",
+    },
+    {
+      id: 3,
+      name: "New Year Party",
+      date: "2021-01-01",
+      venue: "Riversands Incubation hub",
+    },
+  ];
+
+  app.get("/events", async (req, res) => {
+    try {
+      res.send(events);
+    } catch (e) {
+      console.error(e.message);
+    }
+  });
   app.post("/User", async (req, res) => {
     try {
       const newUser = new User({
         name: req.body.name,
         surname: req.body.surname,
-        email: req.body.email
+        email: req.body.email,
       });
-      newUser.save()
-      res.json(newUser)
-    }catch (err) {
-      console.log('err', err)
+      newUser.save();
+      res.json(newUser);
+    } catch (err) {
+      console.error(e);
+      res.status(500).send("Server Error");
     }
   });
-}
+};
 
-module.exports ={appRoute};
+module.exports = { appRoute };
--- tcg-events/server/server.js
+++ tcg-events/server/server.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const { appRoute } = require("./routes/user");
+const cors = require("cors");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
 const db = require("./config/keys").mongoURI;
 mongoose
   .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
